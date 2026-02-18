8577adb3ea38b3cafd831e0e376be8c3c4c0cf6c
meangirl <vijayara@purdue.edu>
2026-02-18 14:16:37 -0500

    2_18_code

--- ServoTest.py
+++ ServoTest.py
@@ -6,19 +6,17 @@ factory = PiGPIOFactory()
 servo = Servo(17, min_pulse_width = 0.0005, max_pulse_width = 0.0025,
               pin_factory=factory)
 
-
-# ----------------------
-# Servo Function
-# ----------------------
-def valve_open():
-    pwm.ChangeDutyCycle(5.8)
+discharge_time = 2 # Valve open time, measured in seconds
+TES_idle_time = 2 #Valve closed time, measured in seconds
 
 
-def valve_close():
-    pwm.ChangeDutyCycle(5)
+
     
     
-#servo.min()
+servo.value = -0.42
+sleep(5)
+servo.value = 0.37
+sleep(5)
 #print("Position at", servo.value)
 #sleep(5)
 #servo.max()
@@ -29,16 +27,13 @@ def valve_close():
 #sleep(5)
 #servo.detach()
 
-try:
-    while True:
-        servo.min()
-        sleep(1)
-        servo.mid()
-        sleep(1)
-        servo.max()
-        sleep(1)
-except KeyboardInterrupt:
-        servo.stop()
+#try:
+    #while True:
+        
+        
+
+#except KeyboardInterrupt:
+        #servo.stop()
 
 
 
